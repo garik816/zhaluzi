6ad0e043ce7d09ca1715df85e8ecf9520210a544
garik816 <garikpost@rambler.ru>
2018-03-01 14:07:24 +0200

    Add files via upload

--- nodemcu.ino
+++ nodemcu.ino
@@ -1,11 +1,5 @@
 #include <ESP8266WiFi.h>
 #include <Ticker.h>
-#include <WiFiUdp.h>    //ntp
-
-int sec = 0;
-int ts = 0;
-int h = 0;
-int m = 0;
 
 ////////////  step motor  //////////////////////////////////////////////////////////
 // declare variables for the motor pins
@@ -15,8 +9,7 @@ int motorPin3 = 4;    // Yellow / Желтый    - 28BYJ48 pin 3
 int motorPin4 = 0;    // Orange / Оранжевый - 28BYJ48 pin 4
 
 // установить скорость шагового двигателя.
-//variable to set stepper speed.
-int motorSpeed = 1200;  
+int motorSpeed = 1200;
 
 int lookup[8] = {
   B01000, B01100, B00100, B00110, B00010, B00011, B00001, B01001};
@@ -27,58 +20,66 @@ int lookup[8] = {
 const char* ssid = "kyivstar wi-fi";
 const char* password = "dreamteam";
 
-unsigned int localPort = 2390;                  //ntp
-IPAddress timeServerIP;                         //ntp
-const char* ntpServerName = "time.nist.gov";    //ntp
-const int NTP_PACKET_SIZE = 48;                 //ntp
-byte packetBuffer[ NTP_PACKET_SIZE];            //ntp
-WiFiUDP udp;                                    //ntp
-
-uint8_t   blink_loop = 0;
-uint8_t   blink_mode = 0;
-int   sensorValue = 0;
-int   sensetive = 25;
-int   level = 500;
+uint8_t blink_loop = 0;
+uint8_t blink_mode = 0;
+int sensorValue = 0;
+int sensetive = 25;
+int level = 500;
 uint8_t i;
 
 Ticker blinker;
-Ticker getWebTime;
+
 const int upButtonPin = 12;
 const int downButtonPin = 14;
 int upButtonState = 0;
 int downButtonState = 0;
 
+const int modeButtonPin = 15;
+int oldModeButton = 0;
+int newModeButton = 0;
+long buttonTime = 0;
+long debounce = 200;
+
+const int encoderAPin = 13;
+const int encoderBPin = 3;
+int oldEncoderPosition = HIGH;
+int newEncoderPosition = HIGH;
+
 // Create an instance of the server
 // specify the port to listen on as an argument
 WiFiServer server(80);
 
-int manual = 0;
+bool manual = 0;
 int motorDirection = 2;
 int val = 2;
+int stepcount = 0;
+
+//512 - оборот
+void movetocw(int stepcount)
+{
+  manual = 1;
+  for (int i = 0; i < stepcount; i++){
+  clockwise();
+  }
+}
+
+void movetoacw(int stepcount)
+{
+  manual = 1;
+  for (int i = 0; i < stepcount; i++){
+  anticlockwise();
+  }
+}
 
 void timerIsr()
 {
-  if(  blink_mode & 1<<(blink_loop&0x07) ) digitalWrite(2, HIGH); 
+  if(blink_mode & 1<<(blink_loop&0x07)) digitalWrite(2, HIGH); 
     else  digitalWrite(2, LOW);
   blink_loop++;
 
   sensorValue = analogRead(A0);
   upButtonState = digitalRead(upButtonPin);
   downButtonState = digitalRead(downButtonPin);
-}
-
-void unixTimeToTime(int ts)
-{
-  sec = ts%86400;
-  ts /= 86400;
-  h = (sec/3600)+2;
-  m = sec/60%60;
-  sec = sec%60;
-}
-
-void getTime()
-{
-unixTimeToTime(ts);
 }
 
 // функция поворачивает мотор против часовой стрелки.
@@ -109,28 +110,64 @@ void setOutput(int out)
   digitalWrite(motorPin4, bitRead(lookup[out], 3));
 }
 
+void modeButtonAction()
+{
+  newModeButton = digitalRead(modeButtonPin);
 
-void   motorAction()
+  if (newModeButton == HIGH && oldModeButton == LOW && millis() - buttonTime > debounce)
   {
-  if(motorDirection == 0 || val == 0)
-    if(upButtonState == HIGH)
-      {motorZero();}
-    else
-      {anticlockwise();}
+    manual = !manual;
+    Serial.print("manual = "); Serial.print(manual); Serial.print("\n");
+    buttonTime = millis();    
+  }
+  oldModeButton = newModeButton;
+}
+
+void encoderAction()
+{
   
-  if (motorDirection == 1 || val == 1)
-    if (downButtonState == HIGH)
-      {motorZero();}
+  newEncoderPosition = digitalRead(encoderAPin);
+  if ((oldEncoderPosition == LOW) && (newEncoderPosition == HIGH))
+  {
+    if (digitalRead(encoderBPin) == LOW)
+    {
+       Serial.print ("left\n");
+       movetoacw(23);
+    }
     else
-      {clockwise();}
-  else
-  {motorZero();}
-        
+    {
+      Serial.print ("right\n");
+      movetocw(23);
+    }
+  }
+  oldEncoderPosition = newEncoderPosition;
+}
+
+void motorAction()
+  {
+  if(manual == 0)
+  {
+    if(motorDirection == 0)
+      if(upButtonState == HIGH)
+        {motorZero();}
+      else
+        {anticlockwise();}
+    
+    if (motorDirection == 1)
+      if (downButtonState == HIGH)
+        {motorZero();}
+      else
+        {clockwise();}
+    else
+    {motorZero();}
+  }        
 return;
 }
 
 void photoresistorAction(int level, int sensetive)
 {
+   if (manual == 0)
+   {
    if (sensorValue < level - sensetive)
     {motorDirection = 1;}
    else if (sensorValue > level + sensetive)
@@ -138,8 +175,8 @@ void photoresistorAction(int level, int sensetive)
    else
     {
       motorDirection = 2;
-      return;
     }
+   }
 }
 
 void motorZero()
@@ -153,16 +190,21 @@ void motorZero()
 void setup()
 {
   Serial.begin(115200);
+  Serial.println("");
+  Serial.println("");
+  Serial.println("start");
 //  delay(10);                        //check
 
   pinMode(2,OUTPUT);
   blink_mode = 0B11111110;  //blink_mode = 0B11111110; //количество срабатываний в период 1 раз из восьми тактов
   blinker.attach(0.250, timerIsr); //blinker.attach(0.125, timerIsr); //0.125 - время одного периода
   
-  getWebTime.attach(15, getTime); //check
-  
   pinMode(upButtonPin, INPUT);
   pinMode(downButtonPin, INPUT);
+
+  pinMode(encoderAPin, INPUT);
+  pinMode(encoderBPin, INPUT);
+  pinMode(modeButtonPin, INPUT);
 
 // двигло
   pinMode(motorPin1, OUTPUT);
@@ -177,129 +219,25 @@ void setup()
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-//    delay(500);                           //check
+    delay(500);                           //check
     Serial.print(".");
   }
   
   Serial.println("WiFi connected");
-  udp.begin(localPort);
-
   
   // Start the server
   server.begin();
-}
-
-unsigned long sendNTPpacket(IPAddress& address)
-{
-  Serial.println("sending NTP packet...");
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  udp.beginPacket(address, 123); //NTP requests are to port 123
-  udp.write(packetBuffer, NTP_PACKET_SIZE);
-  udp.endPacket();
 }
 
 
 void loop()
 {
 
-  WiFi.hostByName(ntpServerName, timeServerIP); 
-
-  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
-  // wait to see if a reply is available
-//  delay(1000);                                 //check
-  
-  int cb = udp.parsePacket();
-//  if (!cb) {                                  //check
-//    Serial.println("no packet yet");
-//  }
-
-  while(!cb){                                   //check
-    delay(1);
-    Serial.println("no packet yet");
-  }
-  else {
-    Serial.print("packet received, length=");
-    Serial.println(cb);
-    // We've received a packet, read the data from it
-    udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
-
-    //the timestamp starts at byte 40 of the received packet and is four bytes,
-    // or two words, long. First, esxtract the two words:
-
-    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-    // combine the four bytes (two words) into a long integer
-    // this is NTP time (seconds since Jan 1 1900):
-    unsigned long secsSince1900 = highWord << 16 | lowWord;
-    Serial.print("Seconds since Jan 1 1900 = " );
-    Serial.println(secsSince1900);
-
-    // now convert NTP time into everyday time:
-    Serial.print("Unix time = ");
-    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-    const unsigned long seventyYears = 2208988800UL;
-    // subtract seventy years:
-    unsigned long epoch = secsSince1900 - seventyYears;
-    // print Unix time:
-    Serial.println(epoch);
-    ts=epoch;
-
-
-    // print the hour, minute and second:
-    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
-    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
-    Serial.print(':');
-    if ( ((epoch % 3600) / 60) < 10 ) {
-      // In the first 10 minutes of each hour, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
-    Serial.print(':');
-    if ( (epoch % 60) < 10 ) {
-      // In the first 10 seconds of each minute, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.println(epoch % 60); // print the second
-  }
-
-
-  
-     
-
-//  sensorValue = analogRead(A0);   //опрос АЦП (фоторезистор)
-//  float voltage = sensorValue * (5.0 / 1023.0); //перевод данных в напряжение
- if (manual == 0)
- {
+  encoderAction();
+  modeButtonAction();
   photoresistorAction(level, sensetive);
- }
- motorAction();
+  motorAction();
 
-  if (h >= 0 && h <= 5){level=100;}
-  if (h >= 6 && h <= 7){level=200;}
-  if (h >= 8 && h <= 9){level=400;}
-  if (h >= 10 && h <= 12){level=600;}
-  if (h >= 13 && h <= 14){level=700;}
-  if (h==15){level=800;}
-  if (h==16){level=500;}
-  if (h==17){level=300;}
-  if (h==18){level=200;}
-  if (h >= 19 && h <= 23){level=100;}
-  
   // Check if a client has connected
   WiFiClient client = server.available();
   if (!client) {
@@ -356,8 +294,6 @@ void loop()
     else if (req.indexOf("/?send_s=9") != -1){sensetive=9;}
     else if (req.indexOf("/?send_s=25") != -1){sensetive=25;}
     else if (req.indexOf("/?send_s=50") != -1){sensetive=50;}
-    
-    else if (req.indexOf("/?send_time=1") != -1){getTime();}
 
   else {
     String f = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html style=\"width:380px\">\r\n";
@@ -366,8 +302,7 @@ void loop()
     f += "<form method=\"get\" action=\"\">";
     f += "<button style=\"width:100px;height:32px\" value=\"0\" type=\"submit\" name=\"send_m\">up</button><br><br>";
     f += "<button style=\"width:100px;height:32px;margin-right:10px\" value=\"2\" type=\"submit\" name=\"send_m\">stop</button>";
-    f += "<button style=\"width:100px;height:32px;margin-right:10px\" value=\"4\" type=\"submit\" name=\"send_m\">auto</button>";
-    f += "<button style=\"width:100px;height:32px\" value=\"1\" type=\"submit\" name=\"send_time\">time</button><br><br>";
+    f += "<button style=\"width:100px;height:32px;margin-right:10px\" value=\"4\" type=\"submit\" name=\"send_m\">auto</button><br><br>";
     f += "<button style=\"width:100px;height:32px\" value=\"1\" type=\"submit\" name=\"send_m\">down</button>";
     f += "</form></fieldset>";
     f += "<fieldset><legend>level</legend>";
@@ -398,12 +333,6 @@ void loop()
     f += sensetive;
     f += "<br>sensor value = ";
     f += sensorValue;
-    f += "<br>Time: ";
-    f += h;
-    f += ":";
-    f += m;
-    f += ":";
-    f += sec;
     f += "<br>voltage value = ";
     float voltage = sensorValue * (3.3 / 1023.0);
     f += voltage;  
@@ -423,8 +352,7 @@ void loop()
     s += "<form method=\"get\" action=\"\">";
     s += "<button style=\"width:100px;height:32px\" value=\"0\" type=\"submit\" name=\"send_m\">up</button><br><br>";
     s += "<button style=\"width:100px;height:32px;margin-right:10px\" value=\"2\" type=\"submit\" name=\"send_m\">stop</button>";
-    s += "<button style=\"width:100px;height:32px;margin-right:10px\" value=\"4\" type=\"submit\" name=\"send_m\">auto</button>";
-    s += "<button style=\"width:100px;height:32px\" value=\"1\" type=\"submit\" name=\"send_time\">time</button><br><br>";
+    s += "<button style=\"width:100px;height:32px;margin-right:10px\" value=\"4\" type=\"submit\" name=\"send_m\">auto</button><br><br>";
     s += "<button style=\"width:100px;height:32px\" value=\"1\" type=\"submit\" name=\"send_m\">down</button>";
     s += "</form></fieldset>";
     s += "<fieldset><legend>level</legend>";
@@ -455,12 +383,6 @@ void loop()
     s += sensetive;
     s += "<br>sensor value = ";
     s += sensorValue;
-    s += "<br>Time: ";
-    s += h;
-    s += ":";
-    s += m;
-    s += ":";
-    s += sec;
     s += "<br>voltage value = ";
     float voltage = sensorValue * (3.3 / 1023.0);
     s += voltage;
